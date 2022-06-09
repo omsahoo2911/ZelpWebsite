cc2ed5061454f6b595aaa9073df8a91847458883
omsahoo2911 <omsahoo2911@gmail.com>
2022-06-09 16:58:27 -0400

    test
    added comment

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+const init = function(){
+    document.getElementById('submit').addEventListener('click', fromState)
+}
+
+function fromState(ev){
+    let a = document.getElementById("search1").value;
+    let b = document.getElementById("search2").value;
+    let c = document.getElementById("search3").value;
+    let d = document.getElementById("search4").value;
+    let e = document.getElementById("search5").value;
+    document.body.innerHTML = a + b +c+d+e;
+}//qwd12we12qweqwe
+
+document.addEventListener('DOMContentLoaded', init)
